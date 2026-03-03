4ecda5241fe1f48a000b5f3427486278884cd127
Jordan Eric Smith <129927731+Jordan-E-S@users.noreply.github.com>
2026-03-02 21:17:23 -0500

    Update DML.sql
    progress

--- DML.sql
+++ DML.sql
@@ -1,153 +1,150 @@
 -- Jordan Smith & Arthur Franklyn
--- ========================================
--- DML Queries for Board Game Library DB
--- Variables are denoted with @variableName
--- ========================================
+-- CS340 Final Project
+-- DML File
+-- Contains all CRUD SQL queries used by the application
+-- Stored procedure versions of some operations are located in pl.sql
 
--- ========================================
--- SELECT (Browse) Queries
--- ========================================
 
--- BoardGames browse (JOIN to show genre name instead of genre ID)
-SELECT bg.boardGameID, bg.title, bg.publisher, bg.minPlayers, bg.maxPlayers, bg.complexity,
-       gg.genre AS genreName
-FROM BoardGames bg
-JOIN GameGenres gg ON bg.GameGenres_gameGenreID = gg.gameGenreID
-ORDER BY bg.title;
+-- =====================================================
+-- GAMEGENRES
+-- =====================================================
 
--- GameGenres browse
-SELECT gameGenreID, genre
-FROM GameGenres
-ORDER BY genre;
+-- READ all genres
+SELECT * FROM GameGenres;
 
--- Patrons browse
-SELECT patronID, firstName, lastName, email, phoneNumber
-FROM Patrons
-ORDER BY lastName, firstName;
-
--- Staff browse
-SELECT staffID, firstName, lastName
-FROM Staff
-ORDER BY lastName, firstName;
-
--- Checkouts browse (JOIN to show patron/staff names)
-SELECT c.checkoutID, c.checkoutDate, c.dueDate, c.returnDate,
-       CONCAT(p.firstName, ' ', p.lastName) AS patronName,
-       CONCAT(s.firstName, ' ', s.lastName) AS staffName,
-       c.Patrons_patronID, c.Staff_staffID
-FROM Checkouts c
-JOIN Patrons p ON c.Patrons_patronID = p.patronID
-JOIN Staff s ON c.Staff_staffID = s.staffID
-ORDER BY c.checkoutID;
-
--- CheckoutItems browse (JOIN to show game title + patron name)
-SELECT ci.checkoutItemID,
-       ci.Checkouts_checkoutID,
-       ci.BoardGames_boardGameID,
-       bg.title AS gameTitle,
-       CONCAT(p.firstName, ' ', p.lastName) AS patronName
-FROM CheckoutItems ci
-JOIN BoardGames bg ON ci.BoardGames_boardGameID = bg.boardGameID
-JOIN Checkouts c   ON ci.Checkouts_checkoutID = c.checkoutID
-JOIN Patrons p     ON c.Patrons_patronID = p.patronID
-ORDER BY ci.checkoutItemID;
-
--- ========================================
--- FK-Friendly Dropdown Queries (Required by rubric)
--- ========================================
-
--- Genres dropdown (BoardGames FK)
-SELECT gameGenreID, genre
-FROM GameGenres
-ORDER BY genre;
-
--- Patrons dropdown (Checkouts FK)
-SELECT patronID, CONCAT(firstName, ' ', lastName) AS patronName
-FROM Patrons
-ORDER BY lastName, firstName;
-
--- Staff dropdown (Checkouts FK)
-SELECT staffID, CONCAT(firstName, ' ', lastName) AS staffName
-FROM Staff
-ORDER BY lastName, firstName;
-
--- BoardGames dropdown (CheckoutItems FK)
-SELECT boardGameID, title
-FROM BoardGames
-ORDER BY title;
-
--- Checkouts dropdown (CheckoutItems FK, show patron name)
-SELECT c.checkoutID, CONCAT(p.firstName, ' ', p.lastName) AS patronName
-FROM Checkouts c
-JOIN Patrons p ON c.Patrons_patronID = p.patronID
-ORDER BY c.checkoutID;
-
--- ========================================
--- INSERT Queries
--- ========================================
-
--- Add a new board game
-INSERT INTO BoardGames (title, publisher, minPlayers, maxPlayers, complexity, GameGenres_gameGenreID)
-VALUES (@titleInput, @publisherInput, @minPlayersInput, @maxPlayersInput, @complexityInput, @genreIDInput);
-
--- Add a new patron
-INSERT INTO Patrons (firstName, lastName, email, phoneNumber)
-VALUES (@firstNameInput, @lastNameInput, @emailInput, @phoneInput);
-
--- Add a new staff member (optional)
-INSERT INTO Staff (firstName, lastName)
-VALUES (@staffFirstNameInput, @staffLastNameInput);
-
--- Add a new genre (optional)
+-- INSERT genre
 INSERT INTO GameGenres (genre)
-VALUES (@genreInput);
+VALUES (?);
 
--- Add a new checkout (optional)
-INSERT INTO Checkouts (checkoutDate, dueDate, returnDate, Patrons_patronID, Staff_staffID)
-VALUES (@checkoutDateInput, @dueDateInput, @returnDateInput, @patronIDInput, @staffIDInput);
+-- DELETE genre
+DELETE FROM GameGenres
+WHERE gameGenreID = ?;
 
--- Add a new checkout item (M:M insert)
-INSERT INTO CheckoutItems (BoardGames_boardGameID, Checkouts_checkoutID)
-VALUES (@boardGameIDInput, @checkoutIDInput);
 
--- ========================================
--- UPDATE Queries
--- ========================================
 
--- Update board game info (entity update)
+-- =====================================================
+-- BOARDGAMES
+-- =====================================================
+
+-- READ board games with genre name (JOIN)
+SELECT 
+    bg.boardGameID,
+    bg.title,
+    bg.publisher,
+    bg.minPlayers,
+    bg.maxPlayers,
+    bg.complexity,
+    bg.GameGenres_gameGenreID,
+    g.genre
+FROM BoardGames bg
+INNER JOIN GameGenres g
+    ON bg.GameGenres_gameGenreID = g.gameGenreID;
+
+-- INSERT board game
+INSERT INTO BoardGames
+(title, publisher, minPlayers, maxPlayers, complexity, GameGenres_gameGenreID)
+VALUES (?, ?, ?, ?, ?, ?);
+
+-- UPDATE board game
 UPDATE BoardGames
-SET title = @titleInput,
-    publisher = @publisherInput,
-    minPlayers = @minPlayersInput,
-    maxPlayers = @maxPlayersInput,
-    complexity = @complexityInput,
-    GameGenres_gameGenreID = @genreIDInput
-WHERE boardGameID = @boardGameIDInput;
+SET title = ?,
+    publisher = ?,
+    minPlayers = ?,
+    maxPlayers = ?,
+    complexity = ?,
+    GameGenres_gameGenreID = ?
+WHERE boardGameID = ?;
 
--- Update patron info (optional)
-UPDATE Patrons
-SET firstName = @firstNameInput,
-    lastName = @lastNameInput,
-    email = @emailInput,
-    phoneNumber = @phoneInput
-WHERE patronID = @patronIDInput;
+-- DELETE board game
+DELETE FROM BoardGames
+WHERE boardGameID = ?;
 
--- Update checkout item (M:M FK update requirement)
-UPDATE CheckoutItems
-SET BoardGames_boardGameID = @boardGameIDInput,
-    Checkouts_checkoutID = @checkoutIDInput
-WHERE checkoutItemID = @checkoutItemIDInput;
 
--- ========================================
--- DELETE Queries
--- ========================================
 
--- Delete patron (entity delete)
+-- =====================================================
+-- PATRONS
+-- =====================================================
+
+-- READ patrons
+SELECT * FROM Patrons;
+
+-- INSERT patron
+INSERT INTO Patrons
+(firstName, lastName, email, phoneNumber)
+VALUES (?, ?, ?, ?);
+
+-- DELETE patron
 DELETE FROM Patrons
-WHERE patronID = @patronIDInput;
+WHERE patronID = ?;
 
--- Delete checkout item (M:M delete without deleting parents)
+
+
+-- =====================================================
+-- STAFF
+-- =====================================================
+
+-- READ staff
+SELECT * FROM Staff;
+
+
+
+-- =====================================================
+-- CHECKOUTS
+-- =====================================================
+
+-- READ checkouts with patron and staff names (JOIN)
+SELECT 
+    c.checkoutID,
+    c.checkoutDate,
+    c.dueDate,
+    c.returnDate,
+    p.firstName AS patronFirstName,
+    p.lastName AS patronLastName,
+    s.firstName AS staffFirstName,
+    s.lastName AS staffLastName
+FROM Checkouts c
+INNER JOIN Patrons p 
+    ON c.Patrons_patronID = p.patronID
+INNER JOIN Staff s 
+    ON c.Staff_staffID = s.staffID;
+
+-- INSERT checkout
+INSERT INTO Checkouts
+(checkoutDate, dueDate, returnDate, Patrons_patronID, Staff_staffID)
+VALUES (?, ?, ?, ?, ?);
+
+
+
+-- =====================================================
+-- CHECKOUTITEMS (M:M Relationship)
+-- =====================================================
+
+-- READ checkout items with related data (JOIN)
+SELECT 
+    ci.checkoutItemID,
+    ci.Checkouts_checkoutID,
+    ci.BoardGames_boardGameID,
+    p.firstName AS patronName,
+    bg.title
+FROM CheckoutItems ci
+INNER JOIN Checkouts c 
+    ON ci.Checkouts_checkoutID = c.checkoutID
+INNER JOIN Patrons p 
+    ON c.Patrons_patronID = p.patronID
+INNER JOIN BoardGames bg 
+    ON ci.BoardGames_boardGameID = bg.boardGameID;
+
+-- INSERT checkout item
+INSERT INTO CheckoutItems
+(BoardGames_boardGameID, Checkouts_checkoutID)
+VALUES (?, ?);
+
+-- UPDATE checkout item
+UPDATE CheckoutItems
+SET BoardGames_boardGameID = ?,
+    Checkouts_checkoutID = ?
+WHERE checkoutItemID = ?;
+
+-- DELETE checkout item
 DELETE FROM CheckoutItems
-WHERE checkoutItemID = @checkoutItemIDInput;
-
-
+WHERE checkoutItemID = ?;
